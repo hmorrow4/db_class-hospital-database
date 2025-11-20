1ed1836e70d6f1b9d517a48db83fcc2c3ab4c2ef
hmorrow4 <147641009+hmorrow4@users.noreply.github.com>
2025-11-20 15:28:26 -0600

    Create Insert_Professors_Script.sql

---
+++ Insert_Professors_Script.sql
@@ -0,0 +1,44 @@
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (1, 'Alice', 'Wong', 'awong@univ.edu', 'Professor', 1);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (2, 'Robert', 'Mills', 'rmills@univ.edu', 'Associate Professor', 1);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (3, 'Karen', 'Stone', 'kstone@univ.edu', 'Professor', 2);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (4, 'Victor', 'Young', 'vyoung@univ.edu', 'Assistant Professor', 2);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (5, 'Elena', 'Turner', 'eturner@univ.edu', 'Professor', 3);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (6, 'Martin', 'Frost', 'mfrost@univ.edu', 'Associate Professor', 3);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (7, 'Linda', 'Price', 'lprice@univ.edu', 'Assistant Professor', 4);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (8, 'James', 'Olsen', 'jolsen@univ.edu', 'Professor', 4);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (9, 'Sarah', 'Nolan', 'snolan@univ.edu', 'Associate Professor', 5);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (10, 'Henry', 'Cole', 'hcole@univ.edu', 'Assistant Professor', 5);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (11, 'Thomas', 'Reed', 'treed@univ.edu', 'Lecturer', 1);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (12, 'Meghan', 'Sharp', 'msharp@univ.edu', 'Professor', 2);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (13, 'Brian', 'Dover', 'bdover@univ.edu', 'Lecturer', 3);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (14, 'Paula', 'Nguyen', 'pnguyen@univ.edu', 'Associate Professor', 4);
+
+INSERT INTO professor (ProfessorID, First_Name, Last_Name, Email, Title, DepartmentID)
+VALUES (15, 'Derek', 'King', 'dking@univ.edu', 'Professor', 5);
